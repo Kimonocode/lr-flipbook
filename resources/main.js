e86a2d5073edbc740a0750dc941f5bf983d1c9e1
kimonocode <ronald.github.dev@gmail.com>
2023-10-01 22:46:13 +0200

    book first step

--- resources/main.js
+++ resources/main.js
@@ -1,4 +1,36 @@
-import './styles.css';
+import {PageFlip} from 'page-flip';
+import './styles.scss';
 
-console.log('hello !');
+document.addEventListener('DOMContentLoaded', function() {
 
+    const pageFlip = new PageFlip(
+        document.getElementById("demoBookExample"),
+        {
+            width: 550, // base page width
+            height: 733, // base page height
+
+            size: "stretch",
+            // set threshold values:
+            minWidth: 315,
+            maxWidth: 800,
+            minHeight: 420,
+            maxHeight: 800,
+
+            maxShadowOpacity: 0.5, // Half shadow intensity
+            showCover: false,
+            mobileScrollSupport: false // disable content scrolling on mobile devices
+        }
+    );
+
+    // load pages
+    pageFlip.loadFromHTML(document.querySelectorAll(".page"));
+
+    
+    document.querySelector(".btn-prev").addEventListener("click", () => {
+        pageFlip.flipPrev(); // Turn to the previous page (with animation)
+    });
+
+    document.querySelector(".btn-next").addEventListener("click", () => {
+        pageFlip.flipNext(); // Turn to the next page (with animation)
+    });
+});
